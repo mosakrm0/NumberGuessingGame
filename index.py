91eaa0a2292f8dbd71dda5b3f62271cf005bd460
mosakrm <mosakrm001@gmail.com>
2023-07-01 01:53:10 +0300

    v1.0

---
+++ index.py
@@ -0,0 +1,32 @@
+import random
+
+OurRandomNumber = random.randrange(1,101)
+
+print("Welcome to the Gussing Number Game !")
+
+while True:
+
+    try:
+        TheGuss =  input("Guss The Number Between [1 - 100] > ")
+        TheGuss = int(TheGuss)
+
+        if TheGuss > 0 and TheGuss < 101:
+         
+            if (TheGuss == OurRandomNumber):
+                print("")
+                print("Correct !")
+                print("The Number was ", OurRandomNumber , " !")
+                break
+            elif TheGuss > OurRandomNumber : 
+                print("Lower")
+            elif TheGuss < OurRandomNumber :
+                print("Higher")
+            else:
+                print("That Not A Number Between [1 - 100]")
+        
+    except ValueError:
+        print("This is not a Number")
+    
+    
+
+   
